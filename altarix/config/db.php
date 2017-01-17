979a474ec15d4a807c43cf4d1fc043cb48af45bb
Boris Shutov <boris.shutov@cognitive.ru>
2017-01-17 21:39:27 +0300

    Moved files

--- altarix/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;port=5432;dbname=altarix',
-    'username' => 'postgres',
-    'password' => 'postgres',
-    'charset' => 'utf8',
-];
